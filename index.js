7282ecf64595fe1bf9da9812f1a43d92f298d6a2
Brendan Pierce <bpierce@e1z3r4p8.42.us.org>
2018-02-08 12:50:28 -0800

    moved files to resources directory

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const authentication = require('./authentication');
-const RepositoryResource = require('./resources/repository');
-const ServerStatus = require('./database_server_get_server_status');
-const ServerConfiguration = require('./database_server_get_server_configuration');
+// const RepositoryResource = require('./resources/repository');
+
+// Filemaker API integrations are stored in ./resources
+const ServerStatus = require('./resources/database_server_get_server_status');
+const ServerConfiguration = require('./resources/database_server_get_server_configuration');
 
 // JUST FOR TESTING
 // THIS MUST CHANGE IT IS HIGHLY UNSERCURE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
